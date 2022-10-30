49ff71c752ea12bc44e536fcecd5432f94a1a825
Maximilian Biebl <maximilian.biebl@mni.thm.de>
2022-10-30 14:12:19 +0100

    set the color scheme + test page
    Former-commit-id: 5b1275c118cc440ba5e5be925ddb1b92819914ab

--- app/src/app/folder/folder.page.ts
+++ app/src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-folder',
@@ -9,10 +10,21 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private alertController: AlertController) {
+  }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 }
