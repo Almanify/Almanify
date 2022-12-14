77f10658e3a025857fd6e47f1ec4edab02ad29a0
Vanessa Nagy <vanessa.nagy@mni.thm.de>
2022-12-14 14:34:22 +0100

    Fetch journey and use correct invite code
    Former-commit-id: 1cb312fcb75fbc0211ca7ee31999da814c5c5578

--- src/app/pages/invite-modal/invite-modal.page.ts
+++ src/app/pages/invite-modal/invite-modal.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {Journey} from '../../data/Journey';
+import {DatabaseService} from '../../services/database.service';
+import firebase from 'firebase/compat/app';
+import Timestamp = firebase.firestore.Timestamp;
 
 @Component({
   selector: 'app-invite-modal',
@@ -10,12 +14,15 @@ export class InviteModalPage implements OnInit {
   id: string;
   inviteCode: number;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private databaseService: DatabaseService) {
     this.route.params.subscribe(params => {
       this.id = params.id;
+      const journey = new Journey(this.id, '', '', '', Timestamp.fromDate(new Date()), Timestamp.fromDate(new Date()), []);
+      this.databaseService.journeyCrudHandler.read(journey).then((j: Journey) => {
+        this.inviteCode = Number(j.inviteCode);
+      });
     });
-    //TODO: Fetch journey to get invite code for display
-    this.inviteCode = 1;
   }
 
   ngOnInit() {
