bfdcf5736757d4796719abcd732d983a6b9f6052
unknown <sven.thomas@mni.thm.de>
2022-12-23 16:01:45 +0100

    new layout + name and currency are changeable
    Former-commit-id: 89759368b7c1749bf757dda5943aa4190d539bdc

--- src/app/pages/options/options.page.ts
+++ src/app/pages/options/options.page.ts
@@ -1,6 +1,11 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
+import {User} from '../../data/User';
+import {DatabaseService} from '../../services/database.service';
+import {AuthenticationService} from '../../services/auth.service';
+import {currencies} from '../../services/helper/currencies';
+
 @Component({
   selector: 'app-options',
   templateUrl: './options.page.html',
@@ -8,16 +13,25 @@ import { OverlayEventDetail } from '@ionic/core/components';
 })
 export class OptionsPage implements OnInit {
   @ViewChild(IonModal) modal: IonModal;
-  name: string;
-  message = 'Informationen wurden erfolgreich bearbeitet';
+  
+  user: User;
+
+  name: any;
   password: any;
   email: any;
   currency: any;
 
-  constructor() {
+  currencies = currencies;
+  buttonDisabled: boolean=true;
+  inEditMode: boolean=false;
+  message = 'Informationen wurden erfolgreich bearbeitet';
+
+  constructor(public authService: AuthenticationService, private databaseService: DatabaseService) {
+    this.user = new User();
   }
 
   ngOnInit() {
+    this.getUserInformation();
   }
 
   cancel() {
@@ -25,7 +39,7 @@ export class OptionsPage implements OnInit {
   }
 
   confirm() {
-    this.modal.dismiss(this.name, 'confirm');
+    this.modal.dismiss(this.user.userName, 'confirm');
   }
 
   onWillDismiss(event: Event) {
@@ -33,5 +47,25 @@ export class OptionsPage implements OnInit {
     if (ev.detail.role === 'confirm') {
       this.message = `Hello, ${ev.detail.data}!`;
     }
+  }
+
+  getUserInformation() {
+    this.databaseService.userCrudHandler.readByID(this.authService.getUserId).then(u => {
+      this.user.id = u.id;
+      this.user.userName = u.userName;
+      this.user.userCurrency = u.userCurrency;
+    });
+
+    this.email = this.authService.getUserEmail;
+  }
+
+  changeEditMode() {
+    this.buttonDisabled = !this.buttonDisabled;
+    this.inEditMode = !this.inEditMode;
+  }
+
+  save() {
+    this.databaseService.userCrudHandler.update(this.user);
+    this.changeEditMode();
   }
 }
